3a9edb5c6048e30db225935dd60b9bf961fe1e97
KellyCristinaAires <k.woys@hotmail.com>
2020-09-27 15:17:02 -0300

    atividade  fraus FC

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace grausFC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Fahrenheit;
+            double Celsius = 97;
+
+            Console.Write("Digite seu valor Fahrenheit: ");
+            Fahrenheit = double.Parse(Console.ReadLine());
+
+            Celsius = (Fahrenheit -32) / 1.8;
+            Console.WriteLine("Fahrenheit para Celsius = {0}", Celsius);
+            Console.Write("\n");
+
+            Console.WriteLine("Pressione Enter para sair!");
+            Console.ReadKey();
+        }
+    }
+}
